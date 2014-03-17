4a319db680e762eb51e786c16ecef1b4e1d7090e
thejw23 <thejw23@gmail.com>
2014-03-17 13:14:15 +0100

    initial commit
    removed database dependency.

--- HelloWorld.php
+++ HelloWorld.php
@@ -2,28 +2,21 @@
 
 class HelloWorld
 {
-    /**
-     * @var PDO
-     */
-    private $pdo;
-
-    public function __construct(PDO $pdo)
+	$storage = NULL;
+	
+    public function __construct()
     {
-        $this->pdo = $pdo;
     }
 
     public function hello($what = 'World')
     {
-        $sql = "INSERT INTO hello VALUES (" . $this->pdo->quote($what) . ")";
-        $this->pdo->query($sql);
+        $this->storage = $what;
         return "Hello $what";
     }
 
 
     public function what()
     {
-        $sql = "SELECT what FROM hello";
-        $stmt = $this->pdo->query($sql);
-        return $stmt->fetchColumn();
+        return $this->storage;
     }
 }
--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -2,40 +2,31 @@
 
 class HelloWorldTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @var PDO
-     */
-    private $pdo;
-
     public function setUp()
     {
-        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
     }
 
     public function tearDown()
     {
-        $this->pdo->query("DROP TABLE hello");
     }
 
     public function testHelloWorld()
     {
-        $helloWorld = new HelloWorld($this->pdo);
+        $helloWorld = new HelloWorld();
 
         $this->assertEquals('Hello World', $helloWorld->hello());
     }
 
     public function testHello()
     {
-        $helloWorld = new HelloWorld($this->pdo);
+        $helloWorld = new HelloWorld();
 
         $this->assertEquals('Hello Bar', $helloWorld->hello('Bar'));
     }
 
     public function testWhat()
     {
-        $helloWorld = new HelloWorld($this->pdo);
+        $helloWorld = new HelloWorld();
 
         $this->assertFalse($helloWorld->what());
 
